918830bf72ed7489cf3baf1909bf40228900d4a9
smflt <smflt87@gmail.com>
2014-09-25 22:47:27 +0900

    'fix'

--- mongocook/recipes/test.rb
+++ mongocook/recipes/test.rb
@@ -1,10 +1,21 @@
-include_recipe "mongodb"
-include_recipe "mongodb::mongo_gem"
+chef_gem 'mongo'
 
-::Chef::Recipe.send(:include, MongoDB::OpsWorksHelper)
+users = []
+admin = node['mongodb']['admin']
 
-replicaset_layer_slug_name = node['opsworks']['instance']['layers'].first
-replicaset_layer_instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
+# If authentication is required,
+# add the admin to the users array for adding/updating
+users << admin if node['mongodb']['config']['auth'] == true
 
-Chef::Log.info replicaset_layer_slug_name
-Chef::Log.info replicaset_layer_instances
+users.concat(node['mongodb']['users'])
+
+# Add each user specified in attributes
+users.each do |user|
+  mongodb_user user['username'] do
+    password user['password']
+    roles user['roles']
+    database user['database']
+    connection node['mongodb']
+    action :add
+  end
+end
