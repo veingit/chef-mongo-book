c4f80e926c4a83d7005b3515a56dc0848d91ef23
smflt <smflt87@gmail.com>
2014-09-25 20:30:09 +0900

    'fix'

--- mongocook/metadata.rb
+++ mongocook/metadata.rb
@@ -4,13 +4,12 @@ maintainer  "smflt"
 license     "Apache 2.0"
 version     "1.0.0"
 
-%w{ ubuntu debian redhat centos fedora scientific }.each do |os|
-  supports os
-end
+depends "mongodb"
 
 recipe "mongocook", "Installs and configures mongocook"
 recipe "mongocook::test", "add test"
 recipe "mongodb::user_management", "mg"
 
-
-depends "mongodb"
+%w{ ubuntu debian redhat centos fedora scientific }.each do |os|
+  supports os
+end
--- mongocook/recipes/test.rb
+++ mongocook/recipes/test.rb
@@ -1,2 +1,7 @@
-include_recipe "mongodb::10gen_repo"
-include_recipe "mongodb::default"
+::Chef::Recipe.send(:include, MongoDB::OpsWorksHelper)
+
+replicaset_layer_slug_name = node['opsworks']['instance']['layers'].first
+replicaset_layer_instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
+
+Chef::Log.info replicaset_layer_slug_name
+Chef::Log.info replicaset_layer_instances
