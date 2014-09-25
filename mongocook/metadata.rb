eb6892a41c19ba3db73d4309e05cf9232ddcf67f
smflt <smflt87@gmail.com>
2014-09-25 19:07:58 +0900

    'fix'

--- mongocook/metadata.rb
+++ mongocook/metadata.rb
@@ -4,7 +4,7 @@ maintainer  "smflt"
 license     "Apache 2.0"
 version     "1.0.0"
 
-depends 'mongodb'
+recipe "mongocook", "mongocook ins"
+recipe "mongocook::test", "add test"
 
-recipe 'mongocook', 'mongocook ins'
-recipe 'mongocook::test', 'add test'
+depends "mongodb"
